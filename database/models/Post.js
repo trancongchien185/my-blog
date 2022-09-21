0203c739a8d608c2f4a8c6ad967f9578ae6cab19
luantt94 <luantt94.uit@gmail.com>
2022-09-21 21:33:36 +0700

    fix error typing

--- database/models/Post.js
+++ database/models/Post.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    content: String,
-    username: String,
-    createdAt: {
-        type: Date,
-        default: new Date()
-    }
+  title: String,
+  subtitle: String,
+  content: String,
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-})
-
-const Post = mongoose.model('Post',PostSchema)
-module.exports = Post
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
--- index.js
+++ index.js
@@ -1,67 +1,68 @@
-const express = require('express');
+const express = require("express");
 
-const expressEdge = require('express-edge')
-const path = require('path');
+const expressEdge = require("express-edge");
+const path = require("path");
 
 const port = 3000;
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
-const Post = require('./database/models/Post');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const Post = require("./database/models/Post");
 
-const createPostController = require('./controllers/createPost')
-const homePageController = require('./controllers/homePage')
-const storePostController = require('./controllers/storePost')
+const createPostController = require("./controllers/createPost");
+const homePageController = require("./controllers/homePage");
+const storePostController = require("./controllers/storePost");
 
-const app = new express()
+const app = new express();
 
-mongoose.connect('mongodb://localhost/node-js-blog')
+mongoose.connect("mongodb://localhost/node-js-blog");
 
-app.use(fileUpload())
-app.use(express.static('public'));
+app.use(fileUpload());
+app.use(express.static("public"));
 app.use(expressEdge.engine);
-app.set('views', path.resolve(__dirname, "views"));
+app.set("views", path.resolve(__dirname, "views"));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const validateCreatePostMiddleware = (req, res, next) => {
+  if (
+    !req.files.image ||
+    !req.body.username ||
+    !req.body.title ||
+    !req.body.subtitle ||
+    !req.body.content
+  ) {
+    return res.redirect("/posts/new");
+  }
+  next();
+};
 
-
-const validateCreatePostMiddleware = (req, res, next) =>{
-    if(!req.files.image || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content){
-       return res.redirect('/posts/new') 
-    }
-    next()
-}
-
-app.use('/posts/store',validateCreatePostMiddleware);
+app.use("/posts/store", validateCreatePostMiddleware);
 
 app.get("/", homePageController);
-app.get("/posts/new", createPostController)
-app.post('/posts/store', storePostController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
 
-
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact')
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
+app.get("/post/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", {
+    post,
+  });
 });
 
-
-app.listen(Post, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-app.get('/posts/new', (req, res) => {
-    res.render("create")
+app.get("/posts/new", (req, res) => {
+  res.render("create");
 });
-
