0093caefc1d902d733b795e96308ff1394c4737e
trancongchien <trancongchien185@gmail.com>
2022-09-25 13:18:19 +0700

    update

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,8 +1,8 @@
 const Post = require('../database/models/Post') 
 
 module.exports = async (req, res) =>{
-    const posts = await Post.find({})
-    console.log(req.session)
+    const posts = await Post.find({}).populate('author');
+   
     res.render('index', {
         posts
     })
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const expressEdge = require("express-edge");
 const express = require("express");
+const edge = require("edge.js");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
@@ -14,7 +15,8 @@ const getPostController = require("./controllers/getPost");
 const createUserController = require("./controllers/createUser");
 const storeUserController = require("./controllers/storeUser");
 const loginController = require("./controllers/login");
-const loginUserController = require('./controllers/loginUser')
+const loginUserController = require('./controllers/loginUser');
+const logoutController = require("./controllers/logout");
 
 const app = new express();
 mongoose.connect("mongodb://localhost/node-js-blog");
@@ -33,20 +35,28 @@ app.use(express.static("public"));
 app.use(expressEdge.engine);
 app.set("views", `${__dirname}/views`);
 
+app.use('*', (req, res, next) => {
+  app.locals.auth = req.session.userId
+  next()
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const storePost = require("./middleware/storePost");
 const auth = require("./middleware/auth");
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated')
 
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", auth, createPostController);
+app.get('/auth/logout',auth, logoutController)
 app.post("/posts/store", auth, storePost, storePostController);
-app.get('/auth/login', loginController);
-app.post('/users/login', loginUserController)
-app.get("/auth/register", createUserController);
-app.post("/users/register", storeUserController);
+app.get('/auth/login', redirectIfAuthenticated,loginController);
+app.post('/users/login', redirectIfAuthenticated,loginUserController)
+app.get("/auth/register", redirectIfAuthenticated,createUserController);
+app.post("/users/register", redirectIfAuthenticated,storeUserController);
+app.use((req, res) => res.render('not-found'));
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
